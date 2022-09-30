98e8b90e5db713553838c5fe424cd6362d3b7fa2
nikusick <91880536+nikusick@users.noreply.github.com>
2022-09-30 21:24:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,26 +6,30 @@
 
 
 int main(int argc, char** argv) {
+    if (argc == 3) {
+        std::ifstream fin(argv[1]);
+        std::ofstream fout(argv[2]);
 
-    std::ifstream fin(argv[1]);
-    std::ofstream fout(argv[2]);
+        std::vector<int> nums;
+        int num;
+        int N;
 
-    std::vector<int> nums;
-    int num;
-    int N;
+        fin >> N;
+        while (fin >> num) {
+            nums.push_back(num);
+        }
 
-    fin >> N;
-    while (fin >> num) {
-        nums.push_back(num);
+        std::sort(nums.begin(), nums.end());
+        auto opponentSum = std::accumulate(nums.begin(), nums.begin() + N / 2, 0);
+        auto ownSum = std::accumulate(nums.begin() + N / 2, nums.end(), 0);
+
+        fout << ownSum - opponentSum;
+
+        fin.close();
+        fout.close();
     }
-
-    std::sort(nums.begin(), nums.end());
-    auto opponentSum = std::accumulate(nums.begin(), nums.begin() + N / 2, 0);
-    auto ownSum = std::accumulate(nums.begin() + N / 2, nums.end(), 0);
-
-    fout << ownSum - opponentSum;
-
-    fin.close();
-    fout.close();
+    else {
+        std::cout << "Must be 2 arguments: input and output files";
+    }
     return 0;
 }
